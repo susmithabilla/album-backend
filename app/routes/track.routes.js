3ccb7d7f507a8fc4acc6cfad8d9da2cd4cccf735
AmruthaReddy97 <a.balasaani@eagles.oc.edu>
2022-05-30 10:51:42 -0500

    api route added for retrieve track api

--- app/routes/track.routes.js
+++ app/routes/track.routes.js
@@ -4,8 +4,11 @@ module.exports = app => {
   // Create a new track 
   //Retrieve All tracks
   router.post("/:albumId/tracks/", tracks.create);
+  
   // Retrieve a single track with id
   router.get("/:albumId/tracks/:id", tracks.findOne);
+   // Retrieve all Lessons for a Tutorial
+   router.get("/:albumId/tracks/", tracks.findAll);
  
   app.use('/api/albums', router);
 };
